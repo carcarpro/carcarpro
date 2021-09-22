ec4074b3cc0038331fabdde264ed9b36e9937acf
zx <zhenxiangwang@xverse.cn>
2021-09-22 13:10:20 +0800

    added g.sh

--- g.sh
+++ g.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-git add -A && git commit -m 'updates'
+MSG=$1 &&
+if [[ $MSG ]]; then
+    echo "Commit message is $MSG"
+    git add -A && git commit -m $MSG
+else
+    echo "No commit message provided, default to ``updates``"
+    git add -A && git commit -m "updates"
